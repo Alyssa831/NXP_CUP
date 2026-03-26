d00c83c08bd44d2f190c536ae7f214ca1d92d182
Alyssa <qd230@cam.ac.uk>
2026-03-26 10:19:56 +0100

    added new code

---
+++ Tmp/nxpcup/Debug/source/semihost_hardfault.d
@@ -0,0 +1,4 @@
+source/semihost_hardfault.o source/semihost_hardfault.d: \
+ ../source/semihost_hardfault.c \
+ C:\Users\asyou\Desktop\Tmp\nxpcup\source\mcux_config.h
+C:\Users\asyou\Desktop\Tmp\nxpcup\source\mcux_config.h:
